06296389f0c1c6ba16849593ea2c2400c3728206
stefanrogic <Stefanrogic@protonmail.com>
2025-06-27 16:15:40 +0200

    Added users and get calls for single and all users.

---
+++ src/main/java/com/example/RestAPI/api/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.example.RestAPI.api.controller;
+import com.example.RestAPI.api.model.User;
+import com.example.RestAPI.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.Optional;
+import java.util.List;
+
+// This annotation tells Spring that this class will handle REST API requests
+@RestController
+public class UserController {
+    // This field holds a reference to the UserService, which provides user-related operations
+    private UserService userService;
+
+    // Constructor for UserController. Spring will automatically provide a UserService instance when creating this controller.
+    public UserController(UserService userService){
+        // Assign the provided UserService to the field so it can be used in the methods below
+        this.userService = userService;
+    }
+
+    // This method handles GET requests to /users and returns a list of all users
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        // Calls the UserService to get all users and returns the list
+        return userService.getUsers();
+    }
+
+    // This method handles GET requests to /user?id=... and returns a single user by their ID
+    @GetMapping("/user")
+    public User getUser(@RequestParam Integer id) {
+        // Calls the UserService to get a user by ID. The result is wrapped in an Optional to handle the case where the user might not exist.
+        Optional<User> user = userService.getUser(id);
+
+        // If a user with the given ID exists, return it
+        if(user.isPresent()) {
+            return user.get();
+        }
+
+        // If no user is found, return null (could also return an error or a special response in a real app)
+        return null;
+    }
+}
---
+++ src/main/java/com/example/RestAPI/service/UserService.java
@@ -0,0 +1,48 @@
+package com.example.RestAPI.service;
+import com.example.RestAPI.api.model.User;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    // This field holds a list of User objects. It acts as our in-memory database for this example.
+    private List<User> userList;
+
+    // Constructor for UserService. This is called when the service is created.
+    public UserService() {
+        // Initialize the userList as an empty ArrayList.
+        userList = new ArrayList<>();
+
+        // Create some sample User objects with id, name, age, and email.
+        User user1 = new User(0, "Admin", 29, "admin@admin.com");
+        User user2 = new User(1, "Jane", 31, "jane@mail.com");
+        User user3 = new User(2, "John", 25, "john@mail.com");
+        User user4 = new User(3, "Jimmy", 45, "jimmy@mail.com");
+
+        // Add all the sample users to the userList.
+        userList.addAll(Arrays.asList(user1,user2,user3,user4));
+    }
+
+    // This method tries to find a user by their ID.
+    // It returns an Optional<User> which will contain the user if found, or be empty if not found.
+    public Optional<User> getUser(Integer id) {
+        Optional<User> optional = Optional.empty(); // Start with an empty Optional
+        for(User user: userList){ // Loop through all users
+            if(id == user.getId()) { // If the user's id matches the given id
+                optional = Optional.of(user); // Put the user in the Optional
+                return optional; // Return the Optional containing the user
+            }
+        }
+        return optional; // If no user is found, return an empty Optional
+    }
+
+    // This method returns the entire list of users.
+    public List<User> getUsers() {
+        return userList;
+    }
+}
